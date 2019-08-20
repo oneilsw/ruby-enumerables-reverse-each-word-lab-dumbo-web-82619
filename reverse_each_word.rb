5d3103edf89bd0b8835edd382f644943efd5c4f7
Oneil Williams <oneilswilliams@gmail.com>
2019-08-20 22:13:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@ def reverse_each_word(string)
   i=0 
   fresh =[]
   while i < new.length 
-    fresh << new.each {|s| s[0].reverse}
+    new.each {|s| s[0].reverse}
     i +=1
-    fresh
+    
   end  
-  fresh 
+  
 end

339be9264cd7eb4819edc272bc92743da6aca827
Oneil Williams <oneilswilliams@gmail.com>
2019-08-20 22:31:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
 def reverse_each_word(string)
   new = []
   string_array =string.split
-  string_array.each do |word| 
-    new << word.reverse
+  string_array.collect do |word| 
+    #new << 
+    word.reverse
     
   end
   
-  new.join(" ") 
+  #new.join(" ")
+  
+  
 end
